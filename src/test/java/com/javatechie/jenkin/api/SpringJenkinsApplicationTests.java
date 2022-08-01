517bae3a378e261884e902468cf7cf92e253337f
aniruddha <aniruddha.chunne@gunadhyasoft.com>
2022-08-01 19:55:02 +0530

    first jenkin project

---
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.javatechie.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@Test
+	void contextLoads() 
+	{
+		logger.info("Test case executing");
+		assertEquals(true, true);
+	}
+
+}
